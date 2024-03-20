8c87144a91e9b387396339ab1ebcc74c52118ab1
princesaubhagyam <prince@saubhagyam.org>
2024-03-20 19:53:07 +0530

    init

---
+++ src/pages/Aboutus.jsx
@@ -0,0 +1,128 @@
+import Footer from "../components/Footer";
+import BreadCrumbCom from "../components/BreadCrumbCom";
+import Navbar from "../components/Navbar";
+import { Box, Container, VStack, Image, Text, Heading } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+const Aboutus = () => {
+  let { search } = useLocation();
+  const searchParams = new URLSearchParams(search);
+  const IsMobileView = searchParams.get("mobile") ?? "false";
+  return (
+    <>
+      {IsMobileView !== "true" && <Navbar />}
+      <Container maxW={"container.xl"} alignContent={"flex-start"}>
+        <BreadCrumbCom second={"About Us"} secondUrl={"/about-us"} />{" "}
+      </Container>
+      <Container maxW={"container.xl"} mb={4} px={0} centerContent>
+        <Box
+          w={"100%"}
+          bgImage={
+            "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/aboutUs.jpg"
+          }
+          bgSize="cover"
+          bgPosition="center"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          mt={"-10px"}
+          py={20}
+          boxShadow={"0px 0px 0px 0px"}
+          height={"550px"}
+          mb={10}
+          // filter="brightness(200%)"
+          // style={{ backdropFilter: "blur(10px)" }}
+        >
+          <Text
+            pb={2}
+            color={"brand.100"}
+            textAlign={"center"}
+            fontSize="7xl"
+            fontWeight="600"
+          >
+            About Us
+          </Text>
+
+          {/* <Text
+            pb={2}
+            color={"brand.100"}
+            textAlign={"center"}
+            textShadow={"1px 1px 2px lightgreen"}
+            fontSize="7xl"
+            fontWeight="black"
+          >
+            About us
+          </Text> */}
+        </Box>
+      
+        <VStack maxW={"6xl"} mt={3} mb={7}>
+          <Text fontWeight={600} fontSize={"2xl"} color={"text.300"}>
+            WHY COTTON ?
+          </Text>
+          <Box color={"text.300"} textAlign={"justify"}>
+            The concern for a life devoid of the use of extremely harmful toxic
+            chemicals, the need for an eco-friendly industrial and agricultural
+            culture and an increasing awareness of depleting natural resources
+            and the consequences therein-these are factors which are shaping the
+            life styles of people world wide. It is in this context that the
+            relevance of cotton becomes important.
+            <br />
+            <br />
+            Cotton is grown and processed without toxic chemicals that can be
+            absorbed easily when in contact with the user's skin. Pesticides,
+            fertilizers and chemicals used to grow and process conventional
+            cotton fabrics may go directly to the users blood stream, which
+            consequently affect's the body's organs and tissues.
+            <br />
+            <br />
+            Besides the naturally soft cotton fabric is a lot more comfortable
+            to use and is available at competitive prices.
+            <br />
+            <br />
+            Take a look at some hard facts given below :
+            <br />
+            <br />
+            <ul style={{ marginLeft: 45 }}>
+              <li>
+                Most of the chemicals used in conventional farming were first
+                developed for warfare!
+              </li>
+              <li>
+                An estimated 25 million people worldwide are poisoned by
+                pesticides every year!
+              </li>
+              <li>
+                25% of the pesticides and fertilizers used in the world are
+                sprayed in conventional cotton crops even though these crops
+                occupy just 3% of the world’s farmland.
+              </li>
+              <li>
+                Over 0.75 kgs of toxic chemicals are used to grow the cotton
+                needed for a conventional cotton sheet set! About 0.5kgs to make
+                a T-shirt and pair of jeans.
+              </li>
+              <li>
+                Some of these chemicals and pesticides are among those
+                classified by the United States - Environmental Protection
+                Agency.
+              </li>
+            </ul>
+            <br />
+            So, here is to a safer, better world for our children and us. Let us
+            make the earth safer for farm workers and preserve the soil for
+            long-term fertility.
+            <br />
+            <br />
+            Our mission and appeal is to encourage more and more people globally
+            to use cotton wear and fabrics.
+          </Box>
+
+          <br />
+        </VStack>
+      </Container>
+      {IsMobileView !== "true" && <Footer />}
+    </>
+  );
+};
+
+export default Aboutus;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,434 @@
+import { useState, useEffect } from "react";
+import Loader from "../components/Loader";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Carousel from "../components/Carousel";
+import CarouselWithLinks from "../components/CarouselWithLinks";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import {
+  FaHeartbeat,
+  FaThumbsUp,
+  FaUniversalAccess,
+  FaUserMd,
+  FaUserPlus,
+  FaBlind,
+  FaHeart,
+} from "react-icons/fa";
+import { MdWaterDrop } from "react-icons/md";
+import { FaGears } from "react-icons/fa6";
+import { CiHeart } from "react-icons/ci";
+
+import ProductListSection from "../components/ProductListSection";
+import {
+  Container,
+  Flex,
+  Image,
+  Heading,
+  Stat,
+  StatNumber,
+  StatHelpText,
+  SimpleGrid,
+  Box,
+  Link,
+  Center,
+  useMediaQuery,
+  Text,
+  Grid,
+  GridItem,
+  LinkBox,
+  LinkOverlay,
+  useBreakpointValue,
+  Card,
+  Skeleton,
+  Button,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
+import client from "../setup/axiosClient";
+import CheckOrSetUDID from "../utils/checkOrSetUDID";
+import { useNavigate, NavLink as RouterLink } from "react-router-dom";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import Testimonials from "../components/testimonials";
+
+const imageInfo = [
+  {
+    src: require("../assets/Home/OC.png"),
+    name: "NON-GMO Product",
+  },
+  {
+    src: require("../assets/Home/natural & Organic.png"),
+    name: "Ethical & Natural",
+  },
+  {
+    src: require("../assets/Home/love.png"),
+    name: "Vegetarian",
+  },
+  {
+    src: require("../assets/Home/best service.png"),
+    name: "Quality you'll Love Guaranteed",
+  },
+];
+
+const Details = [
+  {
+    id: 1,
+    alt_text: "Image1",
+    image: require("../assets/Home/banner 111.jpg"),
+    display_status: true,
+    image_url: "/shop?page=1&category=573",
+    sose_sequence: 1,
+  },
+  {
+    id: 2,
+    alt_text: "Image1",
+    image: require("../assets/Home/bannner 222.jpg"),
+    display_status: true,
+    image_url: "/shop?page=1&category=573",
+    sose_sequence: 2,
+  },
+];
+
+export default function Home() {
+  const [isFullScreen] = useMediaQuery("(min-width: 768px)");
+  const width = useBreakpointValue({ base: "100%", lg: "100%" });
+  const height = useBreakpointValue({ base: "300", lg: "400" });
+  const [banners, setBanners] = useState(Details);
+  const [loading, setLoading] = useState(true);
+  const [isMobile] = useMediaQuery("(max-width: 480px)");
+  const [homeData, setHome] = useState({});
+  // let [isFull] = useMediaQuery("(max-width:1920px)");
+  const [blogs, setBlogs] = useState([]);
+  const isMobiles = width <= 768;
+  const navigate = useNavigate();
+  useEffect(() => {
+    CheckOrSetUDID();
+    getHomePageData();
+    getBlogs();
+  }, []);
+
+  async function getHomePageData() {
+    const response = await client.get("/home");
+    if (response.data.status === true) {
+      setHome(response.data);
+    }
+    setLoading(false);
+  }
+  async function getBlogs() {
+    const params = {};
+    const response = await client.get("/home/blogs/", {
+      params: params,
+    });
+    if (response.data.status === true) {
+      setBlogs(response.data.blogs);
+    }
+  }
+
+  return (
+    <>
+      {/* {loading === true ? (
+        <Center h="100vh" w="100vw" backgroundColor={"bg.500"}>
+          <Loader site={true} />
+        </Center>
+      ) : (
+        <> */}
+      <Navbar />
+      <Container maxW={"container.xl"} px={0}>
+        {loading === true ? (
+          <Skeleton h={489}></Skeleton>
+        ) : (
+          <Carousel banners={banners} />
+        )}
+        {/* <Image w={"100%"} h={489} src={require("../assets/Home/1.jpg")} /> */}
+      </Container>
+
+      <Container
+        maxW={"container.xl"}
+        mb={8}
+        mt={2}
+        px={0}
+        textAlign={"center"}
+      >
+        <Text
+          fontSize={{ base: "xl", sm: "2xl", xl: "2xl" }}
+          fontWeight={500}
+          color={"text.500"}
+          bgColor={"bg.500"}
+          textAlign={{ base: "center", md: "start" }}
+          px={{ base: 2, md: 8 }}
+          py={4}
+          my={7}
+        >
+          About कपा:
+        </Text>
+        <Text
+          color={"text.300"}
+          px={{ base: 15, lg: 20 }}
+          fontSize={{ base: "sm", lg: "lg" }}
+        >
+          A Manufacturer and Exporter of Knitted garments focusing /
+          specialising in Cotton and Bamboo clothing. Over 7 years of experience
+          in working with Top Brands in India. We assure our customers of the
+          highest quality and outstanding service.
+          <br />
+        </Text>
+        <Text
+          color={"text.300"}
+          px={{ base: 15, lg: 20 }}
+          fontSize={{ base: "sm", lg: "lg" }}
+        >
+          Management structure : Our Company is committed to total quality
+          control & timely shipments.
+          <br />
+          <br />
+        </Text>
+        <Text
+          color={"text.300"}
+          px={{ base: 15, lg: 20 }}
+          fontSize={{ base: "sm", lg: "lg" }}
+        >
+          We have professional merchandisers who handle all details of specific
+          customers, which ensures clear - communication & execution of orders .
+          We specialise in the middle to higher end of market . We understand
+          the International Market & the needs of customers . We are regularly
+          making photo & salesmen samples for our customer as per their
+          requirements.
+          <br />
+          <br />
+        </Text>
+
+        <Link
+          fontWeight={700}
+          color={"brand.500"}
+          as={RouterLink}
+          to={"/about-us"}
+          border={"1px"}
+          borderColor={"brand.500"}
+          p={3}
+          borderRadius={"10px"}
+          _hover={{textDecoration:"none",bgColor:"brand.500",color:"white"}}
+        >
+          Read more
+        </Link>
+      </Container>
+
+      <Container mb={5} px={0} maxW={"container.xl"} centerContent>
+        <LazyLoadImage
+          src={require("../assets/Home/cotton mask.jpg")}
+          alt=""
+          style={{
+            opacity: 1,
+            transition: "opacity 0.7s", // Note the corrected syntax here
+          }}
+        />
+
+        <UnorderedList m={3}>
+          <ListItem m={2}>
+            This Face Mask are made with 2 layers of white cotton.
+          </ListItem>
+          <ListItem m={2}>
+            {" "}
+            2 layer filtration system, helps to block harmful dust particles &
+            droplets Anti-pollution.{" "}
+          </ListItem>
+          <ListItem m={2}>
+            {" "}
+            This mask are anti-dust, reusable & washable and Its face coverage
+            is wide and maximum protection with soft elastic ear loops.
+          </ListItem>
+          <ListItem m={2}>
+            This mask is great for protecting your airways from dust, allergens,
+            chemicals and smoke to help you breathe easier and stay germ-free.
+          </ListItem>
+        </UnorderedList>
+
+        <LazyLoadImage
+          src={require("../assets/Home/right wrong wrong.jpg")}
+          alt=""
+          style={{
+            opacity: 1,
+            transition: "opacity 0.7s", // Note the corrected syntax here
+          }}
+        />
+        <LazyLoadImage
+          src={require("../assets/Home/nocolor.jpg")}
+          alt=""
+          style={{
+            opacity: 1,
+            transition: "opacity 0.7s", // Note the corrected syntax here
+          }}
+        />
+        <Flex alignItems={"center"} justifyContent={"space-between"}>
+          <UnorderedList m={3} width={"48%"}>
+            <ListItem m={2}>
+              <b>Care Instructions :-</b> Laundry Wash, In Cool Water, Do Not
+              Use Brush
+            </ListItem>
+            <ListItem m={2}>
+              <b>Fabric :-</b> Cotton Blend
+            </ListItem>
+            <ListItem m={2}>
+              <b>Size :-</b> For Adults
+            </ListItem>
+            <ListItem m={2}>
+              <b>Gender :-</b> Unisex
+            </ListItem>
+            <ListItem m={2}>
+              <b>Colour :-</b> White
+            </ListItem>
+            <ListItem m={2}>
+              <b>Unifit :-</b> Fits all face types.
+            </ListItem>
+          </UnorderedList>
+          <LazyLoadImage
+            src={require("../assets/Home/mask mm.png")}
+            width={445}
+            height={345}
+            alt=""
+            style={{
+              opacity: 1,
+              transition: "opacity 0.7s", // Note the corrected syntax here
+            }}
+          />
+        </Flex>
+        <Link
+          fontWeight={700}
+          color={"brand.500"}
+          as={RouterLink}
+          to={"/"}
+          border={"1px"}
+          borderColor={"brand.500"}
+          p={3}
+          borderRadius={"10px"}
+        >
+          Buy now
+        </Link>
+        <LazyLoadImage
+          w="100%"
+          mx="auto"
+          style={{
+            opacity: 1,
+            transition: "opacity 0.7s", // Note the corrected syntax here
+          }}
+        />
+        <Grid
+          templateColumns={{
+            base: "repeat(2, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(4,1fr)",
+          }}
+          gap={2}
+          my={6}
+          mx={{ md: "15%", base: 3 }}
+        >
+          {imageInfo?.map((data) => (
+            <GridItem mx={5}>
+              <Flex
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <LazyLoadImage
+                  cursor={"pointer"}
+                  transition="all 1s ease"
+                  _hover={{
+                    transform: "scale(1.25)",
+                  }}
+                  src={data.src}
+                  alt={data.name}
+                  style={{
+                    opacity: 1,
+                    transition: "opacity 0.7s",
+                    width: "100px",
+                    // Note the corrected syntax here
+                  }}
+                />
+                {/* <Text textAlign={"center"} fontSize={"14px"} mt={2}>
+                  {data.name}
+                </Text> */}
+              </Flex>
+            </GridItem>
+          ))}
+        </Grid>
+      </Container>
+
+      <Container backgroundColor={"bg.500"} maxW={"container.xl"} py={2}>
+        <SimpleGrid
+          columns={[2, 3, null, 4]}
+          px={6}
+          maxW={"container.xl"}
+          my={6}
+          backgroundColor={"bg.500"}
+          align="center"
+          spacingX={{ base: "10vw", md: "30px" }}
+          spacingY="40px"
+        >
+          <Stat>
+            <StatNumber color="text.300" fontSize={{ base: "3xl", md: "3xl" }}>
+              82062+
+            </StatNumber>
+            <StatHelpText color="gray.600">Ethical Farmers</StatHelpText>
+          </Stat>
+
+          <Stat>
+            <StatNumber color="text.300" fontSize={{ base: "3xl", md: "3xl" }}>
+              8745+
+            </StatNumber>
+            <StatHelpText color="gray.600">Satisfied Clients</StatHelpText>
+          </Stat>
+
+          <Stat>
+            <StatNumber color="text.300" fontSize={{ base: "3xl", md: "3xl" }}>
+              17+
+            </StatNumber>
+            <StatHelpText color="gray.600">Stores</StatHelpText>
+          </Stat>
+
+          <Stat>
+            <StatNumber color="text.300" fontSize={{ base: "3xl", md: "3xl" }}>
+              30+
+            </StatNumber>
+            <StatHelpText color="gray.600">Countries</StatHelpText>
+          </Stat>
+        </SimpleGrid>
+      </Container>
+     
+      <Container maxW={{ base: "100vw", md: "container.xl" }}>
+        <Box
+          w="100%"
+          backgroundSize="100%"
+          backgroundPosition="50% 100%"
+          backgroundRepeat={"no-repeat"}
+        >
+          <Heading
+            color="brand.500"
+            size="lg"
+            mx="auto"
+            align={"center"}
+            my={"5"}
+            pb={"10px"}
+          >
+            SERVING TO THE COUNTRIES
+          </Heading>
+        </Box>
+        <Box display={"flex"} justifyContent={"center"}>
+          <LazyLoadImage
+            src={
+              "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/Map.webp"
+            }
+            w={{ base: "100%", md: "100%" }}
+            alt=""
+            py={4}
+            style={{
+              opacity: 1,
+              transition: "opacity 0.7s", // Note the corrected syntax here
+            }}
+          />
+        </Box>
+      
+      </Container>
+      <Footer />
+      {/* </>
+      )} */}
+    </>
+  );
+}
