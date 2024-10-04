b40a15f9a7b889adde443964d8705029351993e1
dixi1365 <dixi@saubhagyam.org>
2024-10-04 15:28:49 +0530

    api call home page lower section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -96,6 +96,7 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const [isMobile] = useMediaQuery("(max-width: 480px)");
   const [homeData, setHome] = useState({});
+  const [sections, setSections] = useState([]);
   // let [isFull] = useMediaQuery("(max-width:1920px)");
   const [blogs, setBlogs] = useState([]);
   const isMobiles = width <= 768;
@@ -104,6 +105,7 @@ export default function Home() {
     CheckOrSetUDID();
     //getHomePageData();
     getBlogs();
+    getImage();
   }, []);
 
   async function getHomePageData() {
@@ -122,6 +124,15 @@ export default function Home() {
       setBlogs(response.data.blogs);
     }
     setLoading(false);
+  }
+  async function getImage() {
+    const params = {};
+    const response = await client.get("/lower-section", {
+      params: params,
+    });
+    if (response.data.status === true) {
+      setSections(response.data.data);
+    }
   }
 
   return (
@@ -354,14 +365,12 @@ export default function Home() {
             my={"5"}
             pb={"10px"}
           >
-            OUR SERVICES ARE AVAILABLE IN
+             {sections?.length >0 && sections[1].label}
           </Heading>
         </Box>
         <Box display={"flex"} justifyContent={"center"}>
           <LazyLoadImage
-            src={
-              "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/Map.webp"
-            }
+            src={sections?.length > 0 && sections[1]?.images[0].image}
             w={{ base: "100%", md: "100%" }}
             alt=""
             py={4}
