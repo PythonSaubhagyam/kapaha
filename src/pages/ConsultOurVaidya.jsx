4fb8eca0f615afbef8979fea47d13e61322a4391
dixi1365 <dixi@saubhagyam.org>
2024-10-18 16:28:10 +0530

    bug fix

--- src/pages/ConsultOurVaidya.jsx
+++ src/pages/ConsultOurVaidya.jsx
@@ -9,17 +9,21 @@ import {
     Button,
     HStack,
     useToast,
+    Heading,
+    Grid,
+    GridItem
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import checkLogin from "../utils/checkLogin";
 import { useLocation } from "react-router-dom";
 import { HiInformationCircle } from "react-icons/hi";
 import BreadCrumbCom from "../components/BreadCrumbCom";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function ConsultOurVaidya() {
     let { search } = useLocation();
-    const searchParams = new URLSearchParams(search);
-   const IsMobileView = searchParams.get("mobile") ?? "false";
+  const searchParams = new URLSearchParams(search);
+  const IsMobileView = searchParams.get("mobile") ?? "false";
 
     const toast = useToast();
     const navigate = useNavigate();
@@ -42,40 +46,61 @@ export default function ConsultOurVaidya() {
 
     return (
         <>
-           {IsMobileView !== "true" && <Navbar />}
+             {IsMobileView !== "true" && <Navbar />}
             <Container maxW="container.xl">
                 <BreadCrumbCom
                     second={"Consult Our Vaidya"}
                     secondUrl={"/consult-our-vaidya"}
                 />
             </Container>
-            <Box
-                w={"100%"}
-                bgImage={"https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/vaidh-cover.webp"}
-                bgSize="cover"
-                bgPosition="center"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                mt={"-10px"}
-                py={20}
-                boxShadow={"0px 0px 0px 0px"}
-                height={"550px"}
-                mb={10}
-            >
-                <Text
-                    pb={2}
-                    color={"brand.100"}
-                    textAlign={"center"}
-                    textShadow={"0px 1px 50px lightgreen"}
-                    fontSize="7xl"
-                    fontWeight="600"
-                >
-                    Consult Our Vaidya
-                </Text>
-            </Box>
-            <Container maxW={"container.xl"} py={10}>
-                <Flex>
+            <Container maxW="container.xl" px={0}>
+        <Box
+          position="relative" // Set position relative for parent box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          py={20}
+          h={{ lg: "600px", md: "350px" }}
+          w={"100%"}
+          mb={10}
+        >
+          {/* Background Image */}
+          <Box
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            zIndex={1} // Set a lower zIndex for the background image
+            bgImage={
+              "https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/vaidh-cover.webp"
+            }
+            bgSize="cover"
+            bgPosition="center"
+          />
+          {/* Overlay */}
+          <Box
+            position="absolute"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            zIndex={2}
+            bg="rgba(0, 0, 0, 0.5)"
+          />
+
+          <Text
+            color={"#fff"}
+            textAlign={"center"}
+            zIndex={3}
+            fontSize={{ lg: "5xl", md: "2xl" }}
+          >
+            Get Free Consultation with our Vaidya for Gau Adharit Diagnosis
+          </Text>
+        </Box>
+      </Container>
+            <Container maxW={"6xl"} py={15}>
+            <Flex flexDirection={{ base: "column", lg: "row" }}>
                     <Flex direction={"column"} justify={"center"}>
                         <Box my="8">
                             <Text fontSize={"2xl"}>
@@ -186,23 +211,64 @@ export default function ConsultOurVaidya() {
                         />
                     </Box>
                 </Flex>
-                <Flex justify={"center"} my={"5"}>
-                    <Box my="6">
-                        <Button
-                            onClick={navigateToBooking}
-                            to={"/consult-our-vaidya/schedule-appointment"}
-                            borderRadius="25px"
-                            px="16"
-                            py="6"
-                            bg={"brand.200"}
-                            color={"white"}
-                            fontSize={"xl"}
-                            _hover={{ bg: "brand.200" }}
-                        >
-                            Consult With Vaidya
-                        </Button>
-                    </Box>
-                </Flex>
+                <Heading fontSize={"27px"} textAlign={"center"} color={"text.500"}>
+          OUR VAIDYA VIDEO
+        </Heading>
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(2,1fr)",
+          }}
+          gap={7}
+          my={6}
+          mx={{ md: 20, base: 3 }}
+        >
+          <GridItem>
+            <iframe
+              width="100%"
+              height="450"
+              src="https://www.youtube.com/embed/qLs7KIs1iS4"
+              title="Experience the power of ancient wisdom with Gau Based Aushadhi"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </GridItem>
+          <GridItem>
+            <Heading fontSize={"23px"} mb={3} color={"text.500"}>
+              Experience the power of ancient wisdom with Gau Based Aushadhi
+            </Heading>
+            <Text fontSize={"19px"} color={"text.300"}>
+              Experience the power of ancient wisdom with Gaumutra Based
+              Aushadhi – a cherished treasure from our heritage. This natural
+              remedy, distilled from cow urine, offers effective relief from
+              conditions like obesity, urinary disorders, anemia, and even aids
+              in fighting several diseases.
+              <br />
+              Our vaidyas are ever present with free consultation to guide you
+              about the right kind of aushadhi that will be beneficial for your
+              health.
+              <br />
+              Visit us today or order online for this authentic healing
+              solution.
+            </Text>
+            <Button
+              onClick={navigateToBooking}
+              to={"/consult-our-vaidya/schedule-appointment"}
+              borderRadius="25px"
+              px="16"
+              py={5}
+              my={7}
+              bg={"brand.200"}
+              color={"white"}
+              fontSize={"xl"}
+              _hover={{ bg: "brand.200" }}
+            >
+              Consult With Vaidya
+            </Button>
+          </GridItem>
+        </Grid>
                 <Flex m={"10"}>
                     <Image
                         src={"https://forntend-bucket.s3.ap-south-1.amazonaws.com/sose/images/ConsultOurvaidya/Statistics.png"}
@@ -231,6 +297,7 @@ export default function ConsultOurVaidya() {
                     </Box>
                 </Flex>
             </Container>
+            <ScrollToTop/>
             {IsMobileView !== "true" && <Footer />}
         </>
     );
